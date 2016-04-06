b64bf9cd12056d40db24b8e4c9b3c09aeafefc47
Robert Marianski <rob@marianski.com>
2016-04-06 17:30:19 -0400

    wip

--- data/migrations/create-sql-functions.py
+++ data/migrations/create-sql-functions.py
@@ -5,10 +5,17 @@ from TileStache.Goodies.VecTiles.transform import _parse_kt
 import os.path
 import yaml
 
-# Rule = namedtuple(
-#     'Rule',
-#     'calc equals not_equals not_exists set_memberships exists default_rule'
-# )
+
+def format_string_value(s):
+    return "'%s'" % s
+
+
+def format_key(k):
+    if k.startswith('tags->'):
+        val = "tags->'%s'" % (k[len('tags->'):])
+    else:
+        val = format_string_value(k)
+    return val
 
 
 class EqualsRule(object):
@@ -18,7 +25,9 @@ class EqualsRule(object):
         self.value = value
 
     def as_sql(self):
-        return '%s = %s' % (self.column, self.value)
+        return '%s = %s' % (
+            format_key(self.column),
+            format_string_value(self.value))
 
     def columns(self):
         return [self.column]
@@ -31,7 +40,10 @@ class SetRule(object):
         self.values = values
 
     def as_sql(self):
-        return '%s IN ()' % (self.column, ', '.join(self.values))
+        formatted_values = map(format_string_value, self.values)
+        return '%s IN (%s)' % (
+            format_key(self.column),
+            ', '.join(formatted_values))
 
     def columns(self):
         return [self.column]
@@ -46,7 +58,7 @@ class AndRule(object):
         return ' AND '.join([x.as_sql() for x in self.rules])
 
     def columns(self):
-        return sum([x.columns() for x in self.rules])
+        return sum([x.columns() for x in self.rules], [])
 
 
 class OrRule(object):
@@ -58,7 +70,7 @@ class OrRule(object):
         return ' OR '.join([x.as_sql() for x in self.rules])
 
     def columns(self):
-        return sum([x.columns() for x in self.rules])
+        return sum([x.columns() for x in self.rules], [])
 
 
 class NotRule(object):
@@ -73,52 +85,35 @@ class NotRule(object):
         return self.rule.columns()
 
 
-def format_string_value(value):
-    formatted_value = "'%s'" % value
-    return formatted_value
-
-
-def format_calc_value(value):
-    if value.startswith('${') and value.endswith('}'):
-        calc_value = value[2:-1]
+def create_level_filter_rule(filter_level, combinator=AndRule):
+    rules = []
+    for k, v in filter_level.items():
+        rule = create_filter_rule(k, v)
+        rules.append(rule)
+    assert rules, 'No rules specified in level: %s' % filter_level
+    if len(rules) > 1:
+        rule = combinator(rules)
     else:
-        # calc_value = format_string_value(value)
-        calc_value = value
-    return calc_value
+        rule = rules[0]
+    return rule
 
 
 def create_filter_rule(filter_key, filter_value):
     # check for the composite rules first
     if filter_key == 'not':
-        return NotRule(create_filter_rule(filter_value))
+        rule = create_level_filter_rule(filter_value)
+        rule = NotRule(rule)
     elif filter_key == 'all':
-        rules = []
-        for k, v in filter_value.items():
-            rule = create_filter_rule(k, v)
-            rules.append(rule)
-        assert rules, 'all rule specified with no sub rules'
-        if len(rules) > 1:
-            rule = AndRule(rules)
-        else:
-            rule = rules[0]
-        return rule
+        rule = create_level_filter_rule(filter_value)
     elif filter_key == 'any':
-        rules = []
-        for k, v in filter_value.items():
-            rule = create_filter_rule(k, v)
-            rules.append(rule)
-        assert rules, 'all rule specified with no sub rules'
-        if len(rules) > 1:
-            rule = OrRule(rules)
-        else:
-            rule = rules[0]
-        return rule
+        rule = create_level_filter_rule(filter_value, combinator=OrRule)
     else:
         # leaf rules
         if isinstance(filter_value, list):
-            return SetRule(filter_key, filter_value)
+            rule = SetRule(filter_key, filter_value)
         else:
-            return EqualsRule(filter_key, filter_value)
+            rule = EqualsRule(filter_key, filter_value)
+    return rule
 
 
 class Matcher(object):
@@ -132,7 +127,8 @@ class Matcher(object):
     def when_sql_output(self):
         hstore_output = ','.join(
             ['%s=>%s' % (k, v) for k, v in self.output.items()])
-        return 'WHEN %s THEN HSTORE(%s)' % (self.rule.as_sql(), hstore_output)
+        return "WHEN %s THEN HSTORE('%s')" % (
+            self.rule.as_sql(), hstore_output)
 
     def when_sql_min_zoom(self):
         return 'WHEN %s THEN %s' % (self.rule.as_sql(), self.min_zoom)
@@ -159,10 +155,26 @@ def create_case_statement_min_zoom(matchers):
     when_parts = []
     for matcher in matchers:
         when_sql_part = matcher.when_sql_min_zoom()
+        # indent
+        when_sql_part = '    %s' % when_sql_part
         when_parts.append(when_sql_part)
     when_sql = '\n'.join(when_parts)
-    case_sql = 'CASE\n%s\nEND' % when_sql
+    case_sql = 'CASE\n%s\n  END' % when_sql
     return case_sql
+
+
+def create_case_statement_output(matchers):
+    when_parts = []
+    for matcher in matchers:
+        when_sql_part = matcher.when_sql_output()
+        # indent
+        when_sql_part = '    %s' % when_sql_part
+        when_parts.append(when_sql_part)
+    when_sql = '\n'.join(when_parts)
+    case_sql = 'CASE\n%s\n  END' % when_sql
+    return case_sql
+
+Key = namedtuple('Key', 'table key typ')
 
 
 layers = {}
@@ -171,34 +183,37 @@ script_root = os.path.dirname(__file__)
 for layer in ('landuse', 'pois', 'transit', 'water'):
     kind_rules = []
     min_zoom_rules = []
-    file_path = os.path.join(script_root, layer)
+    file_path = os.path.join(script_root, '%s.yaml' % layer)
     with open(file_path) as fh:
         yaml_data = yaml.load(fh)
         matchers = []
         for yaml_datum in yaml_data:
             matcher = create_matcher(yaml_datum)
-            matcher.append(matcher)
+            matchers.append(matcher)
 
-    # TODO handle params!
-    # need to iterate through the matchers
-    # because the columns are grouped by the table on a per matcher basis anyway
-    params = set(sum([x.columns() for x in matchers]))
-    osm_tags = set([Key(table='osm', key='tags', typ='hstore'),
-                    Key(table=None, key='tags', typ='hstore')])
-    params = ((used_params(kind_rules) | used_params(min_zoom_rules))
-              - osm_tags)
+    params = set()
+    for matcher in matchers:
+        columns = matcher.rule.columns()
+        for column in columns:
+            if not column.startswith('tags'):
+                key = Key(table=matcher.table, key=column, typ='text')
+                params.add(key)
+
+    # osm_tags = set([Key(table='osm', key='tags', typ='hstore'),
+    #                 Key(table=None, key='tags', typ='hstore')])
+    # params = ((used_params(kind_rules) | used_params(min_zoom_rules))
+    #           - osm_tags)
+
     # sorted params is nicer to read in the sql
     params = sorted(params)
-    kind_case_statement = create_case_statement_kind(matchers)
+
     min_zoom_case_statement = create_case_statement_min_zoom(matchers)
+    kind_case_statement = create_case_statement_output(matchers)
     layers[layer] = dict(
         params=params,
         kind_case_statement=kind_case_statement,
         min_zoom_case_statement=min_zoom_case_statement,
     )
-
-import sys
-sys.exit(0)
 
 template_name = os.path.join(script_root, 'sql.jinja2')
 environment = Environment(loader=FileSystemLoader('.'))
